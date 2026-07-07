d61f9846e70fa760dec1b7a7f43ae717697368ef
dylan <ddinmassoma@gmail.com>
2026-07-07 11:09:24 +0200

    pagination

--- accueil.php
+++ accueil.php
@@ -8,49 +8,32 @@
 </form>
 
 <?php
-    header('Content-Type: text/html; charset=utf-8');
     if (isset($_POST['submit'])) {
-        // Connect to the database
         $connection_string = new mysqli("127.0.0.1", "root", "", "harmytech_phone");
-        
-        // Escape the search string and trim
-        // all whitespace
         $searchString = mysqli_real_escape_string($connection_string, trim(htmlentities($_POST['search'])));
+        $page = isset($_GET['subpage']) ? (int)$_GET['subpage'] : 1;
+        if ($page < 1) {
+            $page = 1;
+        }
+        $limit = 5;
+        $offset = ($page - 1) * $limit;
 
-        // If there is a connection error, notify
-        // the user, and Kill the script.
         if ($connection_string->connect_error) {
             echo "Failed to connect to Database";
             exit();
         }
-
         
-
-        // We are using a prepared statement with the
-        // search functionality to prevent SQL injection.
-        // So, we need to prepend and append the search
-        // string with percent signs
         $searchString = "%$searchString%";
-
-        // The prepared statement
-        $sql = "SELECT * FROM base_de_donn__e___harmytech___feuille_1 WHERE nom LIKE ?";
-
-        // Prepare, bind, and execute the query
+        $sql = "SELECT * FROM base_de_donn__e___harmytech___feuille_1 WHERE nom LIKE ? LIMIT $limit OFFSET $offset";
         $prepared_stmt = $connection_string->prepare($sql);
         $prepared_stmt->bind_param('s', $searchString);
         $prepared_stmt->execute();
-
-        // Fetch the result
         $result = $prepared_stmt->get_result();
 
         if ($result->num_rows === 0) {
-            // No match found
             echo "No match found";
-            // Kill the script
             exit();
-
         } else {
-            // Process the result(s)
             while ($row = $result->fetch_assoc()) {
                 echo "<b>Nom</b>: ". $row['nom'] . "<br/>";
                 echo "<b>Couleur</b>: ". $row['couleur'] . "<br/>";
@@ -59,9 +42,17 @@
                 echo "<b>Réference constructeur</b>: ". $row['ref constructeur'] . "<br/>";
                 echo "<b>ID</b>: ". $row['id'] . "<br/>";
                 echo ""."<br/>";
+            }
+        }
 
-            } // end of while loop
-        } // end of if($result->num_rows)
-
+        $totalArticles = $connection_string->query("SELECT COUNT(*) FROM base_de_donn__e___harmytech___feuille_1 WHERE nom LIKE '$searchString'")->fetch_row()[0];
+        $totalPages = ceil($totalArticles / $limit);
+        for ($i = 1; $i <= $totalPages; $i++) {
+            if ($i == $page) {
+                echo '<strong>' . $i . '</strong> ';
+            } else {
+                echo '<a href="index.php?page=accueil&subpage=' . $i . '">' . $i . '</a> ';
+            }
+        }
     }
 ?>
--- configuration/database.php
+++ configuration/database.php
@@ -3,5 +3,4 @@ $host = "127.0.0.1";
 $dbname = "harmytech_phone";
 $username = "root";
 $password = "";
-
 ?>
